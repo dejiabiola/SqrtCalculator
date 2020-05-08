60ef2ae61c36279d08d8f60761b44e88974982d7
Adedeji Abiola <adedejiabiola56@gmail.com>
2020-05-08 02:25:44 +0100

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SqrtCalculator.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
